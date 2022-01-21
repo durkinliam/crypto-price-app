70e580b8d21722b41cd59400cad6ff3543aafe52
Liam Durkin <Liam.Durkin@skybettingandgaming.com>
2022-01-21 14:14:48 +0000

    Finished.

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,10 +1,27 @@
+import styled from "styled-components";
+
 export const Coin = ({name, icon, price, symbol}) => {
     return (
-        <div className='coin'>
+        <StyledCoin>
             <h1>Name: {name}</h1>
-
-            <h3>Price: {price}</h3>
+                <CoinImage src={icon}/>
+            <h3>Price: £{price.toFixed(2)}</h3>
             <h3>Symbol: {symbol}</h3>
-        </div>
+        </StyledCoin>
     )
 }
+
+const StyledCoin = styled.div`
+  width: 400px;
+  height: 300px;
+  background-color: #242424FF;
+  color: #FFFFFFFF;
+  box-shadow: #0000003D 0px 3px 8px;
+  border-radius: 10px;
+  margin: 20px;
+  text-align: center;
+`
+
+const CoinImage = styled.img`
+  height: 100px;
+`
